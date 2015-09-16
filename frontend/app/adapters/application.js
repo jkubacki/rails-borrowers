b9b0b0765afb1ef527ca7276899088817fd08eb5
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-16 21:31:44 +0200

    Move host higher

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,6 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  namespace: 'api',
   host: 'http://api.ember-cli-101.com',
+  namespace: 'api',
 });
