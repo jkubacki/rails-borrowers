c6311165363d9ebcff8885c73e54819ad190b16e
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-16 21:36:07 +0200

    CSRF tokens

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,10 @@
 import ActiveModelAdapter from 'active-model-adapter';
+import $ from 'jquery';
 
 export default ActiveModelAdapter.extend({
   host: 'http://api.ember-cli-101.com',
   namespace: 'api',
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  }
 });
