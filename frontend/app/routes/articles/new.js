f99f4386f52a12a48fce4f32cd5a270c1b6719a4
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-19 22:08:22 +0200

    Move default article state to model

--- frontend/app/routes/articles/new.js
+++ frontend/app/routes/articles/new.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('article', {
-      state: 'borrowed',
       friend: this.modelFor('friends/show')
     });
   },
