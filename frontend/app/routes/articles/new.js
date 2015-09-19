f055c87cde25cfabd8882d199c4dbd43cacd2dac
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-19 22:07:51 +0200

    New article model

--- frontend/app/routes/articles/new.js
+++ frontend/app/routes/articles/new.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('article', {
+      state: 'borrowed',
+      friend: this.modelFor('friends/show')
+    });
+  },
 });
