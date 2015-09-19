1acaeaf5cab1a67df3be37a7e94ae7df1d97eafd
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-19 21:38:45 +0200

    Add return

--- frontend/app/routes/articles/index.js
+++ frontend/app/routes/articles/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    this.modelFor('friends.show').get('articles');
+    return this.modelFor('friends.show').get('articles');
   }
 });
