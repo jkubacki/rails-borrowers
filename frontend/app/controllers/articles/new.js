62cfad02eabce312c61967fff9ff97f57ad44c0c
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-20 20:57:51 +0200

    Use computed macros in validation

--- frontend/app/controllers/articles/new.js
+++ frontend/app/controllers/articles/new.js
@@ -1,14 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.description',
-    {
-      get() {
-        return !Ember.isEmpty(this.get('model.description')) &&
-        !Ember.isEmpty(this.get('model.status'));
-      }
-    }
+  hasDescription: Ember.computed.notEmpty('model.description'),
+  hasStatus: Ember.computed.notEmpty('model.status'),
+  isValid: Ember.computed.and(
+    'hasDescription',
+    'hasStatus'
   ),
   actions: {
     save() {
