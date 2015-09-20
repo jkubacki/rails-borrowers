8c9ce5c9d6dff98d039a6ac8100d325cf1fe938a
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-20 19:56:49 +0200

    total_articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920165948) do
+ActiveRecord::Schema.define(version: 20150920175525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 20150920165948) do
     t.string   "last_name"
     t.string   "twitter"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "total_articles", default: 0, null: false
   end
 
 end
