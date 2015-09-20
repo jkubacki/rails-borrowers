abbceebf1d91ce17f69fe2859759cd2bfdde5680
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-20 20:38:46 +0200

    Remove adapters host. Use our rails api.

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -2,7 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 import $ from 'jquery';
 
 export default ActiveModelAdapter.extend({
-  host: 'http://api.ember-cli-101.com',
   namespace: 'api/v2',
   headers: {
     "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
