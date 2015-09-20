8da130a18ebe94b433f0789ff3402712f53e3e31
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-20 19:11:52 +0200

    Add API V2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   mount EmberCLI::Engine => 'ember-tests' if Rails.env.development?
   root 'application#index'
+  namespace :api do
+    resources :articles, except: [:new, :edit]
+    resources :friends,  except: [:new, :edit]
+    namespace :v2 do
+      resources :friends,  except: [:new, :edit]
+      resources :articles, except: [:new, :edit]
+    end
+  end
   get '/*path' => 'application#index'
 end
