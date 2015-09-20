21b69f5787b48e041d045f7d0151e8d132cf4f40
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-20 18:56:03 +0200

    Add notes to articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920163449) do
+ActiveRecord::Schema.define(version: 20150920165441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150920163449) do
     t.integer  "friend_id"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.text     "notes",       default: ""
   end
 
   create_table "friends", force: :cascade do |t|
