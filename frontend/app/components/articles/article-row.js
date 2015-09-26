d1eec79d78f55062ee9e073bdf75f1d2f1e0db41
Jakub Kubacki <kubacki.jk@gmail.com>
2015-09-27 00:32:22 +0200

    Autosave article

--- frontend/app/components/articles/article-row.js
+++ frontend/app/components/articles/article-row.js
@@ -5,8 +5,11 @@ export default Ember.Component.extend({
   article: null, // passed-in
   articleStates: null, // passed-in
   actions: {
-    saveArticle(article) {
-      this.sendAction('save', article);
+    saveArticle() {
+      var article = this.get('article');
+      if (article.get('hasDirtyAttributes')) {
+        this.sendAction('save', article);
+      }
     }
   }
 });
